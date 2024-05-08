f1ccefd70b3a3646bd02b73dcce77056deea2eb8
gsunkersett <gaurangs@hotmail.com>
2024-05-08 16:06:05 -0700

    state related stuff

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,20 +1,34 @@
+import React, { useState } from 'react';
 import Button from "./Button";
-let productCount = 0;
-function displayFormattedProductCount() {
-    return productCount > 0 ? productCount : 'Zero';
-}
+
+
+
 
 function ProductDetails(props) {
 
+    //array destructing syntax. useState returns array of 2, first is a variable whose value can change
+    //and second is a function
+    let [productCount, updateCount] = useState(0)
+    
+    function displayFormattedProductCount() {
+        return productCount > 0 ? productCount : 'Zero';
+    }
+
+    let incrementProductCount = function() {
+        updateCount(++productCount);
+    }
+    let decrementProductCount = function() {
+        updateCount(--productCount);
+    }
     let badgeClass = "badge-margin-left-240 badge ";
     badgeClass += props.isAvailable === true ? 'bg-success' : 'bg-danger';
- 
+
     return (
         <div className="d-flex align-items-center justify-content-start mt-1">
             <h6 className="font-weight-bold my-2" style={{ 'marginRight': 30 }}>${props.price}</h6>
-            <Button>-</Button>
+            <Button eventHandler={decrementProductCount}>-</Button>
             <span style={{ padding: '0px 14px', 'fontSize': 13 }}>{displayFormattedProductCount()}</span>
-            <Button>+</Button>
+            <Button eventHandler={incrementProductCount}>+</Button>
             <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable'}</span>
         </div >
     )
