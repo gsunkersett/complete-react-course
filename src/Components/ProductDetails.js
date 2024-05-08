5d0d5bf28edee07424f7b49724074d027ec31f65
gsunkersett <gaurangs@hotmail.com>
2024-05-07 17:33:24 -0700

    splitting components

---
+++ src/Components/ProductDetails.js
@@ -0,0 +1,23 @@
+import Button from "./Button";
+let productCount = 0;
+function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : 'Zero';
+}
+
+function ProductDetails(props) {
+
+    let badgeClass = "badge-margin-left-240 badge ";
+    badgeClass += props.isAvailable === true ? 'bg-success' : 'bg-danger';
+ 
+    return (
+        <div className="d-flex align-items-center justify-content-start mt-1">
+            <h6 className="font-weight-bold my-2" style={{ 'marginRight': 30 }}>${props.price}</h6>
+            <Button>-</Button>
+            <span style={{ padding: '0px 14px', 'fontSize': 13 }}>{displayFormattedProductCount()}</span>
+            <Button>+</Button>
+            <span className={badgeClass}>{props.isAvailable ? 'Available' : 'Unavailable'}</span>
+        </div >
+    )
+}
+
+export default ProductDetails
